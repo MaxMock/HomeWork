2e368cc5fc1257598a5235c758c4f4e750b1c22d
Phongphichan Wichianphaisan <phongphichan.wi@gmail.com>
2018-01-29 15:05:38 +0700

    HomeWork

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Form, Icon, Input, Button, Checkbox, Card } from 'antd';
+import './App.css';
+
+class App extends Component {
+  render() {
+
+    const FormItem = Form.Item;
+
+    return (
+        <div className="App">
+        
+            <h1>Welcome to my application</h1>
+            <Card title="Sing In" style={{ width: 300, margin: 'auto' }}>
+              <Form className="login-form">
+                <FormItem>
+                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                </FormItem>
+                <FormItem>
+                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                      Sign in
+                    </Button>
+                    <a className="login-form-forgot" href="">Forgot password</a>
+                </FormItem>
+                <hr></hr>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                      Sign up
+                    </Button>
+                </FormItem>
+              </Form>
+            </Card>
+        </div>
+    );
+  }
+}
+
+export default App;
